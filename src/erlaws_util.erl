21816e2638b5ed483bb569953a081ed548ae804a
Kazuhiro Ogura <rgoura@karesansui-project.info>
2010-08-15 03:13:26 +0900

    Change url_encode to strictly obey AWS requirements.

--- src/erlaws_util.erl
+++ src/erlaws_util.erl
@@ -61,12 +61,13 @@ url_encode_char([X | T], Acc) when X >= $A, X =< $Z ->
     url_encode_char(T, [X | Acc]);
 url_encode_char([X | T], Acc) when X == $-; X == $_; X == $. ->
     url_encode_char(T, [X | Acc]);
-url_encode_char([32 | T], Acc) ->
-    url_encode_char(T, [$+ | Acc]);
+%url_encode_char([32 | T], Acc) ->
+%    url_encode_char(T, [$+ | Acc]);
 url_encode_char([X | T], Acc) ->
     url_encode_char(T, [$%, d2h(X bsr 4), d2h(X band 16#0f) | Acc]);
 			url_encode_char([], Acc) ->
 			       Acc.
 
 d2h(N) when N<10 -> N+$0;
-d2h(N) -> N+$a-10.
+%d2h(N) -> N+$a-10.
+d2h(N) -> N+$A-10.
